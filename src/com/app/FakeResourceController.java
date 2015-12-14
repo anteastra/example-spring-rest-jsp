4da0c9eab67456474befa07cc6f648905dae57b8
anteastra <anteastra@gmail.com>
2015-12-15 00:18:14 +0600

    make /fakeresource return list in xml form

--- src/com/app/FakeResourceController.java
+++ src/com/app/FakeResourceController.java
@@ -14,56 +14,63 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.domain.EntityList;
 import com.domain.FakeResource;
 
 @RestController
 @RequestMapping("/fakeresource")
 public class FakeResourceController {
-	
+
 	private static List<FakeResource> resList = new ArrayList<>();
-	
+
 	static {
-		resList.add(new FakeResource(1, "first_res", "It is a first resource!"));
-		resList.add(new FakeResource(2, "second_res", "just another second resource"));
+		resList.add(
+				new FakeResource(1, "first_res", "It is a first resource!"));
+		resList.add(new FakeResource(2, "second_res",
+				"just another second resource"));
 	}
 
 	@RequestMapping(value = "/{resourceId}", method = RequestMethod.GET)
-    public ResponseEntity<FakeResource> getFakeResource(@PathVariable Long resourceId) {
-        for (FakeResource resource: resList) {
-        	if (resource.getId() == resourceId) {
-        		return new ResponseEntity<FakeResource>(resource, HttpStatus.OK);
-        	}
-        }
-        return new ResponseEntity<FakeResource>(HttpStatus.NOT_FOUND);
-    }
-	
-	@RequestMapping(method = RequestMethod.GET)
-	Collection<FakeResource> returnAllFakeResources() {
-		return resList;
+	public ResponseEntity<FakeResource> getFakeResource(
+			@PathVariable Long resourceId) {
+		for (FakeResource resource : resList) {
+			if (resource.getId() == resourceId) {
+				return new ResponseEntity<FakeResource>(resource,
+						HttpStatus.OK);
+			}
+		}
+		return new ResponseEntity<FakeResource>(HttpStatus.NOT_FOUND);
 	}
-	
+
+	@RequestMapping(method = RequestMethod.GET)
+	EntityList<FakeResource> returnAllFakeResources() {
+		EntityList<FakeResource> listOfFakeResources = new EntityList<>(resList);
+		return listOfFakeResources;
+	}
+
 	@RequestMapping(method = RequestMethod.POST)
 	ResponseEntity<?> addFakeResource(@RequestBody FakeResource input) {
-		
-		FakeResource newRes = new FakeResource(getFakeResourceNextId(), input.getName(), input.getDescription());
+
+		FakeResource newRes = new FakeResource(getFakeResourceNextId(),
+				input.getName(), input.getDescription());
 		resList.add(newRes);
 		HttpHeaders httpHeaders = new HttpHeaders();
-		httpHeaders.setLocation(ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newRes.getId()).toUri());
+		httpHeaders.setLocation(ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(newRes.getId()).toUri());
 		return new ResponseEntity<>(null, httpHeaders, HttpStatus.CREATED);
 
 	}
 
-
 	private long getFakeResourceNextId() {
-		
+
 		long result = 0;
-		for (FakeResource resource: resList) {
-        	if (resource.getId() >= result) {
-        		result = resource.getId() + 1; 
-        	}
-        }
-		
+		for (FakeResource resource : resList) {
+			if (resource.getId() >= result) {
+				result = resource.getId() + 1;
+			}
+		}
+
 		return result;
 	}
-	
+
 }
--- src/com/domain/FakeResource.java
+++ src/com/domain/FakeResource.java
@@ -5,7 +5,7 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlAccessType;
 
-@XmlRootElement(name = "Encoder")
+@XmlRootElement(name = "FakeResource")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class FakeResource {
 
