b82b01a416264ddb0c72edd04306fc752fd15705
anteastra <anteastra@gmail.com>
2015-12-14 09:14:02 +0600

    Commit Four something was done and it works with JSON
    http://localhost:8080/rest/kfc/brands/kfc-kampar
    http://localhost:8080/rest/greeting

--- src/com/app/GreetingController.java
+++ src/com/app/GreetingController.java
@@ -4,6 +4,7 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.domain.Greeting;
@@ -14,8 +15,8 @@ public class GreetingController {
 	private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    @RequestMapping(value="/greeting", produces={"application/json", "application/xml"}, consumes={"application/json", "application/xml"})
+    public @ResponseBody Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
